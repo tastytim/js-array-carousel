0083a5d24c3122ccb7ebdbb83f37ab9efea42c7c
tastytim <tastytimgm@gmail.com>
2021-10-14 21:39:09 +0200

    commit 3

--- app.js
+++ app.js
@@ -23,44 +23,78 @@ const containerLeftImgs = document.querySelector(".container-img-left");
 const containerRightImgs = document.querySelector(".container-img-right");
 
 for (let i = 0; i < items.length; i++) {
-
-
   let activeClassOpacity = "";
   let activeClassOverlay = "";
-
 
   if (i === 0) {
     activeClassOverlay = "overlay";
     activeClassOpacity = "opacityuno";
   }
 
-
-const primoTesto = 
-`<div class="imgs-left ${activeClassOpacity}">
-<img src="${items[i]}" alt="img-0${i}" />
+  const primoTesto = `<div class="container-img ${activeClassOpacity}">
+<div class="imgs-left">
+<img src="${items[i]}" alt="img-${i}" />
 </div>
-<div class="text ${activeClassOpacity}">
+<div class="text">
 <h1>${title[i]}</h1>
 <p>
-  ${text[i]}
+${text[i]}
 </p>
+</div>
 </div>`;
 
-const secondoTesto = `<div class="imgs-right">
+  const secondoTesto = `<div class="imgs-right">
 <img src="${items[i]}" alt="img-right-0${i}" />
-<div class="${activeClassOverlay}"></div>
-</div><i class="fas fa-chevron-circle-up fa-chevron-circle-up-custom"></i>
-<i class="fas fa-chevron-circle-up fa-chevron-circle-down-custom"></i>`;
+<div class="select-overlay ${activeClassOverlay}"></div>
+</div>`;
 
-containerLeftImgs.innerHTML += primoTesto;
-containerRightImgs.innerHTML += secondoTesto;
+  containerLeftImgs.innerHTML += primoTesto;
+  containerRightImgs.innerHTML += secondoTesto;
 }
 
+const arrowUp = document.querySelector(".fa-chevron-circle-up-custom");
+const arrowDown = document.querySelector(".fa-chevron-circle-down-custom");
 
+arrowUp.addEventListener("click", function () {
+  const containerImg = document.querySelector(".opacityuno");
+  const imgOverlay = document.querySelector(".overlay");
 
-const buttonUp = document.querySelector("fa-chevron-circle-up-custom");
-const buttonUp = document.querySelector("fa-chevron-circle-down-custom");
+  contatore--;
+  if (contatore < 0) {
+    contatore = items.length - 1;
+  }
 
-buttonUp.addEventListener("click", function(){
-   
+  containerImg.classList.remove("opacityuno");
+  imgOverlay.classList.remove("overlay");
+
+  const allImg = document.querySelectorAll(".container-img");
+  const allOverlay = document.querySelectorAll(".select-overlay");
+
+  const newImg = allImg[contatore];
+  const newOverlay = allOverlay[contatore];
+
+  newImg.classList.add("opacityuno");
+  newOverlay.classList.add("overlay");
+});
+
+arrowDown.addEventListener("click", function () {
+  const containerImg = document.querySelector(".opacityuno");
+  const imgOverlay = document.querySelector(".overlay");
+
+  contatore++;
+  if (contatore > items.length - 1) {
+    contatore = 0;
+  }
+
+  containerImg.classList.remove("opacityuno");
+  imgOverlay.classList.remove("overlay");
+
+  const allImg = document.querySelectorAll(".container-img");
+  const allOverlay = document.querySelectorAll(".select-overlay");
+
+  const newImg = allImg[contatore];
+  const newOverlay = allOverlay[contatore];
+
+  newImg.classList.add("opacityuno");
+  newOverlay.classList.add("overlay");
 });
